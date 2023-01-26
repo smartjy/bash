5f82e97848bb1099de656576ee39210533bddecf
smartjy <jyson@superb-ai.com>
2023-01-26 19:11:24 +0900

    access 90 days rotate script

--- aws/rotate-access-key.sh
+++ aws/rotate-access-key.sh
@@ -12,13 +12,13 @@ case $PLATFORM in
 esac
 
 DATE_FORMAT='+%Y-%m-%dT%H:%M:%S'
-INACTIVE_DATE=$($DATE -d '95 days ago' $DATE_FORMAT)
+INACTIVE_DATE=$($DATE -d '90 days ago' $DATE_FORMAT)
 
 # for AWS CLI 
 AWS_OPTS="--no-cli-pager --output text"
 AWS_PROFILE="$1"
 # Default aws profile: suite-dev
-[ -z $AWS_PROFILE ] && AWS_PROFILE='suite-dev'
+[ -z $AWS_PROFILE ] && AWS_PROFILE='suite-prod'
 
 # for informations
 echo -e ""
@@ -27,44 +27,57 @@ echo -e "### 90 day ago date for inactive = [$INACTIVE_DATE]"
 
 # Functions
 slack-message() {
-  YOUR_WEBHOOK_URL='https://hooks.slack.com/services/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' # dev-aws-state-change
+  # WEBHOOK_URL='' # dev-aws-state-change
   curl -X POST -H 'Content-type: application/json' --data \
   '{
     "icon_url": "https://a.slack-edge.com/80588/img/api/aws.png",
     "text": ":aws: *'"$AWS_PROFILE"'* @'"$1"' `'"$2"'` '"$3"'",
-  }' $YOUR_WEBHOOK_URL
+  }' $WEBHOOK_URL
 }
 
 list-users() {
-  aws iam list-users $AWS_OPTS --query 'Users[*].[UserName]'
+  aws iam list-users $AWS_OPTS --profile $AWS_PROFILE --query 'Users[*].[UserName]'
 }
 list-access-keys() {
-  aws iam list-access-keys $AWS_OPTS --user-name $1 $2
+  aws iam list-access-keys $AWS_OPTS --profile $AWS_PROFILE --user-name $1 $2
 }
 update-access-keys() {
-  aws iam update-access-key $AWS_OPTS --user-name $1 --access-key-id $2 --status Inactive 
+  aws iam update-access-key $AWS_OPTS --profile $AWS_PROFILE --user-name $1 --access-key-id $2 --status Inactive 
 }
 delete-access-keys() {
-  aws iam delete-access-key $AWS_OPTS --user-name $1 --access-key-id $2
+  aws iam delete-access-key $AWS_OPTS --profile $AWS_PROFILE --user-name $1 --access-key-id $2
 }
-# Start delete iam access key
+
+echo -e "Start inactive iam access key \n"
 for users in $(list-users); do
-  if [[ $users == "test-rotate"* ]]; then
+  if [ $users != "superbai-phantomai" ] && [ $users != "circleci" ] && [ $users != "auto-label" ]; then
     # echo "$users"
     OLD_ACCESS_KEY_QL='AccessKeyMetadata[?CreateDate<=`'$INACTIVE_DATE'`].[AccessKeyId]'
     for accesskey in $(list-access-keys $users "--query $OLD_ACCESS_KEY_QL"); do
       # echo $accesskey
       if update-access-keys $users $accesskey; then
         echo "Change the state of the previous access key to inactive. $users $accesskey"
-        slack-message "$users" "$accesskey" ":warning: Active access key is older than 90 days!! Status changed active :arrow_right: inactive"
-        sleep 5
+        slack-message "$users" "$accesskey" ":warning: older than 90 days!! Status changed to :arrow_right: \`inactive\`"
+        echo ""
+        sleep 1
       else
         echo "$users $accesskey inactive failed"
       fi
+    done
+  fi
+done
+
+echo -e "Start delete iam access key \n"
+for users in $(list-users); do
+  if [ $users != "superbai-phantomai" ] && [ $users != "circleci" ] && [ $users != "auto-label" ]; then    
+    # echo "$users"
+    OLD_ACCESS_KEY_QL='AccessKeyMetadata[?CreateDate<=`'$INACTIVE_DATE'`].[AccessKeyId]'
+    for accesskey in $(list-access-keys $users "--query $OLD_ACCESS_KEY_QL"); do
+      # echo $accesskey
       if delete-access-keys $users $accesskey; then
-        delete-access-keys $users $accesskey
         echo "Delete the inactive access key. $users $accesskey"
-        slack-message "$users" "$accesskey" ":bangbang: Inactive access keys have been deleted"
+        slack-message "$users" "$accesskey" ":x: Inactive access keys have been \`deleted\`"
+        echo ""
       else
         echo "$users $accesskey delete failed"
       fi
